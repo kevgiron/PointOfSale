efe1566c4bb7c82f424f9146c79bcb170342828d
Unknown <kgiron@mail.csuchico.edu>
2017-03-30 20:57:11 -0700

    fixed tax bug

--- sales.js
+++ sales.js
@@ -4,33 +4,33 @@ function addItem()
 {
   var newItem;
   newItem = document.getElementById("price").value;
-  //IF newItem is not a number
-  // THEN show an alert: "Enter price as a number"
-  //OTHERWISE,
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
-  // update a cookie called "preTax" with the value of runningTotal
+  if(isNaN(newItem)){
+  window.alert("Enter price as a number: ");
+  }
+  else{
+  newItem = Number(newItem);
+  runningTotal = runningTotal + newItem;
+  var dollars;
+  dollars = asCurrency(runningTotal);
+  document.getElementById("subtotal").innerHTML = dollars;
+  document.getElementById("price").value = "";
+  setCookie("preTax", runningTotal);
+  }
 }
 
-//takes a number and gives a string with the number displayed as USD currency
 function asCurrency(val)
 {
   return "$" + val.toFixed(2);
 }
 
-//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function getCookie(cname) {
+
+function getCookie(cname){
     var name = cname + "=";
     var ca = document.cookie.split(';');
     for(var i = 0; i < ca.length; i++) {
@@ -43,4 +43,15 @@ function getCookie(cname) {
         }
     }
     return "";
+}
+
+function calculateReceipt(){
+  var receiptSubtotal = getCookie("preTax");
+  receiptSubtotal = Number(receiptSubtotal);
+  var receiptTax = receiptSubtotal * 0.075;
+  var receiptTotal = receiptSubtotal + receiptTax;
+  document.getElementById("sub").innerHTML = asCurrency(receiptSubtotal);
+  document.getElementById("tax").innerHTML = asCurrency(receiptTax);
+  document.getElementById("tot").innerHTML = asCurrency(receiptTotal);
+
 }
